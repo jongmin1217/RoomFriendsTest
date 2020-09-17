6f6d01515c0dda29b1c7bce1e75a96cbffc401f0
jongmin1217 <syj408886@gamil.com>
2020-09-17 18:40:50 +0900

    layout 생성

--- app/src/main/java/com/example/roomfriendstest/activity/MainActivity.java
+++ app/src/main/java/com/example/roomfriendstest/activity/MainActivity.java
@@ -1,26 +1,38 @@
 package com.example.roomfriendstest.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.roomfriendstest.R;
 
 public class MainActivity extends AppCompatActivity implements MainView{
-    private MainPresenter presenter;
 
-    @BindView(R.id.text)
-    TextView text;
+    @BindView(R.id.imgSearch)
+    ImageView imgSearch;
+    @BindView(R.id.btnRemove)
+    ImageButton btnRemove;
+
+
+    private MainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        imgSearch.setColorFilter(Color.parseColor("#919191"));
+        btnRemove.setColorFilter(Color.parseColor("#919191"));
+
 
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                 .permitDiskReads()
@@ -29,11 +41,11 @@ public class MainActivity extends AppCompatActivity implements MainView{
 
         presenter = new MainPresenter(this);
 
-        presenter.userInfo("a");
+        //presenter.userInfo("a");
     }
 
     @Override
     public void result(String result) {
-        text.setText(result);
+        //text.setText(result);
     }
 }
